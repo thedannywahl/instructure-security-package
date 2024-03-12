c09b2c60777d85196cea9ad1f789ef322afc2922
Danny Wahl <danny@iywahl.com>
2024-03-12 11:45:55 -0600

    add mdx component support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import withMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withMDX()({
+	experimental: {
+		mdxRs: true,
+	},
+	compiler: {
+		emotion: true,
+	},
+	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+	reactStrictMode: true,
+});
 
 export default nextConfig;
